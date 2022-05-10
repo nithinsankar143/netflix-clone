81f8b51e4d64d1e09bff61eba6802d009a6d02ea
Nithin Sankar <nithinsankar143@gmail.com>
2022-05-10 12:21:59 +0530

    Apply CSS for getDetails

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -1,8 +1,23 @@
 const MovieDetails = ({ data }) => {
   return (
     <div className="MovieDetails">
-      <div>{data.id}</div>
-      <div>{data.original_title}</div>
+      <div>
+        <img
+          className="imagestyle"
+          src={`https://image.tmdb.org/t/p/w300/${data.backdrop_path}`}
+          alt=" "
+          height={"100%"}
+          width={"100%"}
+        ></img>
+      </div>
+      <div>
+        <div>{data.id}</div>
+        <div>{data.original_title}</div>
+        <div>{data.original_language}</div>
+        <div>{data.overview}</div>
+        <div>{data.popularity}</div>
+        <div>{data.release_date}</div>
+      </div>
     </div>
   );
 };
